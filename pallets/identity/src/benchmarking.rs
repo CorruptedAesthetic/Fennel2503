355d38f6522933d82a02fc0b92a27542eb8a5cd1
CorruptedAesthetic <neurosx@DESKTOP-AH3IE72>
2025-05-11 00:39:13 -0400

    identitypalletcargobuildsucc

--- pallets/identity/src/benchmarking.rs
+++ pallets/identity/src/benchmarking.rs
@@ -2,83 +2,76 @@
 
 #![cfg(feature = "runtime-benchmarks")]
 
+use crate::{Config};
 use frame_benchmarking::v2::*;
-use frame_system::RawOrigin;
-use frame_support::{traits::ConstU32, BoundedVec};
-
-use crate::{Call, Config, Pallet as Identity};
-use super::mock_runtime::Test;
-use crate as pallet_identity;
 
 #[benchmarks(
-    // Force T::MaxSize = 1024 so that Linear<1, 1024> parses
     where
-        T: Config<MaxSize = ConstU32<1024>>,
+        T: Config<MaxSize = frame_support::traits::ConstU32<1024>>,
 )]
 mod benchmarks {
     use super::*;
+    use crate::{Call, Pallet};
+    use frame_system::RawOrigin;
+    use frame_support::BoundedVec;
+    use frame_benchmarking::{BenchmarkError, whitelisted_caller};
 
     #[benchmark]
     fn create_identity() -> Result<(), BenchmarkError> {
         let who: T::AccountId = whitelisted_caller();
-        #[extrinsic_call] _(RawOrigin::Signed(who.clone()));
-        let last = Identity::<T>::identity_number().saturating_sub(1);
-        assert_eq!(Identity::<T>::identity_list(last), Some(who));
+        #[extrinsic_call]
+        Pallet::<T>::create_identity(RawOrigin::Signed(who.clone()));
+        let last = Pallet::<T>::identity_number().saturating_sub(1);
+        assert_eq!(Pallet::<T>::identity_list(last), Some(who));
         Ok(())
     }
 
     #[benchmark]
     fn revoke_identity() -> Result<(), BenchmarkError> {
         let who: T::AccountId = whitelisted_caller();
-        // pre-create
-        Identity::<T>::create_identity(RawOrigin::Signed(who.clone()).into())?;
-        let last = Identity::<T>::identity_number().saturating_sub(1);
-
-        #[extrinsic_call] _(RawOrigin::Signed(who.clone()), last);
-        assert!(Identity::<T>::identity_list(last).is_none());
+        Pallet::<T>::create_identity(RawOrigin::Signed(who.clone()).into())?;
+        let last = Pallet::<T>::identity_number().saturating_sub(1);
+        #[extrinsic_call]
+        Pallet::<T>::revoke_identity(RawOrigin::Signed(who.clone()), last);
+        assert!(Pallet::<T>::identity_list(last).is_none());
         Ok(())
     }
 
     #[benchmark]
     fn add_or_update_identity_trait(l: Linear<1, 1024>) -> Result<(), BenchmarkError> {
         let who: T::AccountId = whitelisted_caller();
-        Identity::<T>::create_identity(RawOrigin::Signed(who.clone()).into())?;
-        let last = Identity::<T>::identity_number().saturating_sub(1);
-
+        Pallet::<T>::create_identity(RawOrigin::Signed(who.clone()).into())?;
+        let last = Pallet::<T>::identity_number().saturating_sub(1);
         let key: BoundedVec<u8, T::MaxSize> = vec![0u8; l as usize].try_into().unwrap();
         let val: BoundedVec<u8, T::MaxSize> = vec![1u8; l as usize].try_into().unwrap();
-
-        #[extrinsic_call] _(RawOrigin::Signed(who.clone()), last, key.clone(), val.clone());
-        assert_eq!(Identity::<T>::identity_trait_list(last, key), val);
+        #[extrinsic_call]
+        Pallet::<T>::add_or_update_identity_trait(RawOrigin::Signed(who.clone()), last, key.clone(), val.clone());
+        assert_eq!(Pallet::<T>::identity_trait_list(last, key), val);
         Ok(())
     }
 
     #[benchmark]
     fn remove_identity_trait(l: Linear<1, 1024>) -> Result<(), BenchmarkError> {
         let who: T::AccountId = whitelisted_caller();
-        Identity::<T>::create_identity(RawOrigin::Signed(who.clone()).into())?;
-        let last = Identity::<T>::identity_number().saturating_sub(1);
-
+        Pallet::<T>::create_identity(RawOrigin::Signed(who.clone()).into())?;
+        let last = Pallet::<T>::identity_number().saturating_sub(1);
         let key: BoundedVec<u8, T::MaxSize> = vec![0u8; l as usize].try_into().unwrap();
         let val: BoundedVec<u8, T::MaxSize> = vec![1u8; l as usize].try_into().unwrap();
-        Identity::<T>::add_or_update_identity_trait(
+        Pallet::<T>::add_or_update_identity_trait(
             RawOrigin::Signed(who.clone()).into(),
             last,
             key.clone(),
             val,
         )?;
-
-        #[extrinsic_call] _(RawOrigin::Signed(who.clone()), last, key.clone());
-        assert_eq!(
-            Identity::<T>::identity_trait_list(last, key),
-            BoundedVec::<u8, T::MaxSize>::default()
-        );
+        #[extrinsic_call]
+        Pallet::<T>::remove_identity_trait(RawOrigin::Signed(who.clone()), last, key.clone());
+        assert_eq!(Pallet::<T>::identity_trait_list(last, key), BoundedVec::<u8, T::MaxSize>::default());
         Ok(())
     }
 
     impl_benchmark_test_suite!(
-        Identity,
+        Pallet,
         crate::mock::new_test_ext(),
-        Test
+        crate::mock::Test
     );
 }
